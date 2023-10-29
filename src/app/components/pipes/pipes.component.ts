88e1acd1e968d06fbfaae9696d352425b99f8728
Robson Araujo <roobson.araujo02@gmail.com>
2023-10-29 18:19:40 -0300

    Used component `pipe`

--- src/app/components/pipes/pipes.component.ts
+++ src/app/components/pipes/pipes.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.scss']
+  styleUrls: ['./pipes.component.scss'],
 })
-export class PipesComponent {
+export class PipesComponent implements OnInit {
+  someText = 'TESTANDO O PIPE OPERATOR';
+  today = new Date();
 
+  constructor() {}
+
+  ngOnInit() {}
 }
