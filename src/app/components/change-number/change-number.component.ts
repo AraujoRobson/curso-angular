6fdd901cf81e2e57bd908eb3bf313f60ba73f6ce
Robson Araujo <roobson.araujo02@gmail.com>
2023-10-29 11:20:32 -0300

    Change number

--- src/app/components/change-number/change-number.component.ts
+++ src/app/components/change-number/change-number.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-change-number',
   templateUrl: './change-number.component.html',
-  styleUrls: ['./change-number.component.scss']
+  styleUrls: ['./change-number.component.scss'],
 })
-export class ChangeNumberComponent {
+export class ChangeNumberComponent implements OnInit {
+  @Output() changeNumber: EventEmitter<any> = new EventEmitter();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleClick() {
+    this.changeNumber.emit();
+  }
 }
