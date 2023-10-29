376b94fdebb93e77f6973fb0d0c398938fe64e6b
Robson Araujo <roobson.araujo02@gmail.com>
2023-10-29 11:30:43 -0300

    Utilization of `*ngFor`

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
-  styleUrls: ['./list-render.component.scss']
+  styleUrls: ['./list-render.component.scss'],
 })
-export class ListRenderComponent {
+export class ListRenderComponent implements OnInit {
+  animals = [
+    { name: 'Max', type: 'Dog' },
+    { name: 'Leo', type: 'Cat' },
+    { name: 'Riley', type: 'Dog' },
+    { name: 'Bob', type: 'Horse' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
